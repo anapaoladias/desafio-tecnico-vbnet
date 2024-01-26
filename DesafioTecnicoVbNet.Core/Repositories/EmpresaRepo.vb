eb6e1aa4f882e068d93d2948957c4bc2ab9025a7
anapaola <anapaolads@gmail.com>
2024-01-25 23:31:25 -0300

    Criação do repositorio para manipulação da Empresa

--- DesafioTecnicoVbNet.Core/Repositories/EmpresaRepo.vb
+++ DesafioTecnicoVbNet.Core/Repositories/EmpresaRepo.vb
@@ -1,13 +1,154 @@
 ﻿Imports System.Data.SqlClient
 
 Public Class EmpresaRepo
-    Public Function ObterTodos() As DataTable
+    Public Function ObterTodos(Optional nome As String = Nothing, Optional cnpj As String = Nothing) As DataTable
         Using dataAccess As New DataAccess()
             dataAccess.OpenConnection()
 
-            Dim sql As String = "SELECT * FROM Empresa"
-            Return dataAccess.GetDataTable(sql)
+            Dim sql As String = "SELECT * FROM Empresa WHERE 1 = 1"
+
+            If Not String.IsNullOrEmpty(nome) Then
+                nome = $"%{nome}%"
+                sql &= " AND Nome like @Nome"
+            End If
+
+            If Not String.IsNullOrEmpty(cnpj) Then
+                sql &= " AND CNPJ = @CNPJ"
+            End If
+
+            Dim parametros As New List(Of SqlParameter)()
+
+            If Not String.IsNullOrEmpty(nome) Then
+                parametros.Add(New SqlParameter("@Nome", nome))
+            End If
+
+            If Not String.IsNullOrEmpty(cnpj) Then
+                parametros.Add(New SqlParameter("@CNPJ", cnpj))
+            End If
+
+            Return dataAccess.GetDataTable(sql, parametros)
         End Using
     End Function
 
+    Public Function ObterPorID(id As Integer) As DataTable
+        Using dataAccess As New DataAccess()
+            dataAccess.OpenConnection()
+
+            Dim sql As String = "SELECT * FROM Empresa WHERE Id = @EmpresaID"
+            Dim parametro As New SqlParameter("@EmpresaID", id)
+            Return dataAccess.GetDataTable(sql, New List(Of SqlParameter)() From {parametro})
+        End Using
+    End Function
+
+    Public Sub Adicionar(nome As String, CNPJ As String, associados As List(Of Integer))
+        Using dataAccess As New DataAccess()
+            Try
+                dataAccess.OpenConnection()
+                dataAccess.BeginTransaction()
+
+                Dim sqlEmpresa As String = "INSERT INTO Empresa (Nome, CNPJ) VALUES (@Nome, @CNPJ); SELECT SCOPE_IDENTITY()"
+
+                Dim parametrosEmpresa As New List(Of SqlParameter)()
+                parametrosEmpresa.Add(New SqlParameter("@Nome", nome))
+                parametrosEmpresa.Add(New SqlParameter("@CNPJ", CNPJ))
+
+                Dim empresaID As Integer = CInt(dataAccess.ExecuteScalar(sqlEmpresa, parametrosEmpresa))
+
+                If associados IsNot Nothing AndAlso associados.Count > 0 Then
+                    Dim sqlVincularAssociado As String = "INSERT INTO Empresa_Associado (empresaId, associadoId) VALUES (@EmpresaID, @AssociadoID)"
+
+                    Dim parametroEmpresaID As New SqlParameter("@EmpresaID", empresaID)
+
+                    For Each associadoID As Integer In associados
+                        Dim parametroAssociadoID As New SqlParameter("@AssociadoID", associadoID)
+                        dataAccess.ExecuteNonQuery(sqlVincularAssociado, New List(Of SqlParameter)() From {parametroEmpresaID, parametroAssociadoID})
+                    Next
+                End If
+
+                dataAccess.CommitTransaction()
+            Catch ex As Exception
+                dataAccess.RollbackTransaction()
+                Throw
+            Finally
+                dataAccess.CloseConnection()
+            End Try
+        End Using
+    End Sub
+
+    Public Sub Atualizar(id As Integer, nome As String, CNPJ As String, associados As List(Of Integer))
+        Using dataAccess As New DataAccess()
+            Try
+                dataAccess.OpenConnection()
+                dataAccess.BeginTransaction()
+
+                Dim sql As String = "UPDATE Empresa SET Nome = @Nome, CNPJ = @CNPJ WHERE Id = @EmpresaID"
+
+                Dim parametros As New List(Of SqlParameter)()
+                parametros.Add(New SqlParameter("@Nome", nome))
+                parametros.Add(New SqlParameter("@CNPJ", CNPJ))
+                parametros.Add(New SqlParameter("@EmpresaID", id))
+
+                dataAccess.ExecuteNonQuery(sql, parametros)
+
+                If associados IsNot Nothing AndAlso associados.Count > 0 Then
+                    Dim sqlDesvincularTodos As String = "DELETE From Empresa_Associado Where EmpresaId = @EmpresaID"
+                    Dim parametroEmpresaID As New SqlParameter("@EmpresaID", id)
+                    dataAccess.ExecuteNonQuery(sqlDesvincularTodos, New List(Of SqlParameter)() From {parametroEmpresaID})
+
+                    Dim sqlVincular As String = "INSERT INTO Empresa_Associado (empresaId, associadoId) VALUES (@EmpresaID, @AssociadoID)"
+
+                    For Each associadoID As Integer In associados
+                        Dim parametroAssociadoID As New SqlParameter("@AssociadoID", associadoID)
+                        dataAccess.ExecuteNonQuery(sqlVincular, New List(Of SqlParameter)() From {parametroEmpresaID, parametroAssociadoID})
+                    Next
+                End If
+
+                dataAccess.CommitTransaction()
+            Catch ex As Exception
+                dataAccess.RollbackTransaction()
+                Throw
+            Finally
+                dataAccess.CloseConnection()
+            End Try
+        End Using
+    End Sub
+
+    Public Sub Excluir(id As Integer)
+        Using dataAccess As New DataAccess()
+            Try
+                dataAccess.OpenConnection()
+                dataAccess.BeginTransaction()
+
+                Dim paramId As New SqlParameter("@Id", id)
+
+                Dim sqlExcluir As String = "
+                    DELETE From Empresa_Associado Where EmpresaId = @Id;
+                    DELETE From Empresa Where Id = @Id"
+
+                dataAccess.ExecuteNonQuery(sqlExcluir, New List(Of SqlParameter)() From {paramId})
+
+                dataAccess.CommitTransaction()
+            Catch ex As Exception
+                dataAccess.RollbackTransaction()
+                Throw
+            Finally
+                dataAccess.CloseConnection()
+            End Try
+        End Using
+    End Sub
+
+    Public Function ObterAssociadosVinculados(EmpresaID As Integer) As DataTable
+        Using dataAccess As New DataAccess()
+            dataAccess.OpenConnection()
+
+            Dim sql As String = "
+                select a.Id, a.Nome, a.CPF, a.DataNascimento
+                from Associado a
+                join Empresa_Empresa ea on a.Id = ea.AssociadoId
+                where ea.EmpresaId = @EmpresaId
+                "
+            Dim parametro As New SqlParameter("@EmpresaID", EmpresaID)
+            Return dataAccess.GetDataTable(sql, New List(Of SqlParameter)() From {parametro})
+        End Using
+    End Function
 End Class
--- DesafioTecnicoVbNet.Web/Associados/Lista.aspx
+++ DesafioTecnicoVbNet.Web/Associados/Lista.aspx
@@ -1,44 +1,46 @@
 ﻿<%@ Page Title="Associados" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Lista.aspx.vb" Inherits="DesafioTecnicoVbNet.Web.Associados.Lista" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-   <div class="container mt-5">
-            <h2>Lista de Associados</h2>
-            
-            <div class="row mb-3">
-                <div class="col-md-4">
-                    <label for="txtFiltroNome">Nome:</label>
-                    <asp:TextBox ID="txtFiltroNome" runat="server" CssClass="form-control" MaxLength="200"></asp:TextBox>
-                </div>
-                <div class="col-md-4">
-                    <label for="txtFiltroCPF">CPF:</label>
-                    <asp:TextBox ID="txtFiltroCPF" runat="server" CssClass="form-control" MaxLength="11"></asp:TextBox>
-                </div>
-                <div class="col-md-4">
-                    <label for="txtFiltroDataNascimento">Data de Nascimento:</label>
-                    <asp:TextBox ID="txtFiltroDataNascimento" runat="server" CssClass="form-control" placeholder="DD/MM/AAAA"></asp:TextBox>
-                </div>
-                <div class="col-md-4 mt-3">
-                    <asp:Button ID="btnPesquisar" runat="server" Text="Pesquisar" CssClass="btn btn-primary" OnClick="btnPesquisar_Click" />
-                    <asp:Button ID="btnNovo" runat="server" Text="Novo" CssClass="btn btn-outline-dark" OnClick="btnNovo_Click" />
-                </div>
-            </div>
+    <div class="container mt-5">
+        <h2>Lista de Associados</h2>
 
-            <asp:GridView ID="GridViewAssociados" runat="server" 
-                AutoGenerateColumns="False" 
-                OnRowCommand="GridViewAssociados_RowCommand">
-                <Columns>
-                    <asp:BoundField DataField="Id" HeaderText="ID" SortExpression="Id" />
-                    <asp:BoundField DataField="Nome" HeaderText="Nome" SortExpression="Nome" />
-                    <asp:BoundField DataField="CPF" HeaderText="CPF" SortExpression="CPF" />
-                    <asp:BoundField DataField="DataNascimento" HeaderText="Data de Nascimento" SortExpression="DataNascimento" DataFormatString="{0:dd/MM/yyyy}" />
-                    <asp:TemplateField HeaderText="Ações">
-                        <ItemTemplate>
-                            <asp:Button ID="btnEditar" runat="server" Text="Editar" CssClass="btn btn-warning btn-sm mr-1" CommandName="Editar" CommandArgument='<%# Eval("Id") %>' />
-                            <asp:Button ID="btnDetalhes" runat="server" Text="Detalhar" CssClass="btn btn-info btn-sm mr-1" CommandName="Detalhes" CommandArgument='<%# Eval("Id") %>' />
-                            <asp:Button ID="btnExcluir" runat="server" Text="Excluir" CssClass="btn btn-danger btn-sm" OnClientClick="return confirm('Tem certeza que deseja excluir?');" CommandName="Excluir" CommandArgument='<%# Eval("Id") %>' />
-                        </ItemTemplate>
-                    </asp:TemplateField>
-                </Columns>
-            </asp:GridView>
+        <div class="row mb-3">
+            <div class="col-md-4">
+                <label for="txtFiltroNome">Nome:</label>
+                <asp:TextBox ID="txtFiltroNome" runat="server" CssClass="form-control" MaxLength="200"></asp:TextBox>
+            </div>
+            <div class="col-md-4">
+                <label for="txtFiltroCPF">CPF:</label>
+                <asp:TextBox ID="txtFiltroCPF" runat="server" CssClass="form-control" MaxLength="11"></asp:TextBox>
+            </div>
+            <div class="col-md-4">
+                <label for="txtFiltroDataNascimento">Data de Nascimento:</label>
+                <asp:TextBox ID="txtFiltroDataNascimento" runat="server" CssClass="form-control" placeholder="DD/MM/AAAA"></asp:TextBox>
+            </div>
         </div>
+        <div class="row mb-3">
+            <div class="col-md-4 mt-3">
+                <asp:Button ID="btnPesquisar" runat="server" Text="Pesquisar" CssClass="btn btn-primary" OnClick="btnPesquisar_Click" />
+                <asp:Button ID="btnNovo" runat="server" Text="Novo" CssClass="btn btn-outline-dark" OnClick="btnNovo_Click" />
+            </div>
+        </div>
+
+        <asp:GridView ID="GridViewAssociados" runat="server"
+            AutoGenerateColumns="False"
+            OnRowCommand="GridViewAssociados_RowCommand">
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="ID" SortExpression="Id" />
+                <asp:BoundField DataField="Nome" HeaderText="Nome" SortExpression="Nome" />
+                <asp:BoundField DataField="CPF" HeaderText="CPF" SortExpression="CPF" />
+                <asp:BoundField DataField="DataNascimento" HeaderText="Data de Nascimento" SortExpression="DataNascimento" DataFormatString="{0:dd/MM/yyyy}" />
+                <asp:TemplateField HeaderText="Ações">
+                    <ItemTemplate>
+                        <asp:Button ID="btnEditar" runat="server" Text="Editar" CssClass="btn btn-warning btn-sm mr-1" CommandName="Editar" CommandArgument='<%# Eval("Id") %>' />
+                        <asp:Button ID="btnDetalhes" runat="server" Text="Detalhar" CssClass="btn btn-info btn-sm mr-1" CommandName="Detalhes" CommandArgument='<%# Eval("Id") %>' />
+                        <asp:Button ID="btnExcluir" runat="server" Text="Excluir" CssClass="btn btn-danger btn-sm" OnClientClick="return confirm('Tem certeza que deseja excluir?');" CommandName="Excluir" CommandArgument='<%# Eval("Id") %>' />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
 </asp:Content>
